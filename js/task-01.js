afc63c821a5ac655898e9b59e03a34f0d7f4afc6
Reizlina Tanya <reizlina.tanya@gmail.com>
2022-08-16 14:46:28 +0300

    firs task done

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const categories = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((element) => {
+  const listItem = element.querySelectorAll("li");
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${listItem.length}`);
+});
